f6e8fcebf6793992c96b6ec67f40ba62e6a76792
Evgeniy Dogadaev <dregore@gmail.com>
2019-07-19 14:36:21 +0200

    Add comments

--- index.php
+++ index.php
@@ -16,12 +16,16 @@ $router->get('/api/goods', function() {
 });
 
 //Place new order with status new
+//Goods ids sends in body with key ids comma separated
+//Example ids:  1,3,7,13
 $router->post('/api/orders/add', function() {
     $home = new \App\Controllers\HomeController();
     return $home->addOrder();
 });
 
 //Update order
+//Use order id in request url and order sum in body with key total in json
+//Example: {"total":5516.60}
 $router->put('/api/orders/pay/{d}', function($orderId) {
     $home = new \App\Controllers\HomeController();
     return $home->payOrder($orderId);
